1513b8d6d5182cbf2083b0bd16f60682eba8dc80
pepemememaster <yztdynzsm9@privaterelay.appleid.com>
2026-05-23 05:11:23 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ from datetime import datetime
 # CONFIG
 # ====================================
 
-BOT_TOKEN = "8644117212:AAGJULoVWuf0TQbFSZRw_wcZI2ODSpeoTF4"
+BOT_TOKEN = "8644117212:AAEBI7xTEIzc4NgYxNhhiNeluHG9nWwu0Hw"
 CHAT_ID = "5034825126"
 
 TAKE_PROFIT = 60
